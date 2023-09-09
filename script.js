8d0e02734145328cafbd60d04f8ca37ac76cfff0
Deeksha Gupta <95583212+deeksha2011@users.noreply.github.com>
2023-09-09 17:07:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const inputtdl = document.querySelector('.textarea')
+const buttontdl = document.querySelector('.buttoninput')
+const listtdl = document.querySelector('.todolist')
+
+function clickButton(e) {
+    e.preventDefault()
+    addTodo()
+}
+
+// adding todoList
+function addTodo() {
+    const itemall = document.createElement('div')
+    itemall.classList.add('itemall')
+
+    const item = document.createElement('p')
+    item.classList.add('item')
+    item.innerText = inputtdl.value
+    itemall.appendChild(item)
+
+    if (inputtdl.value === '') return
+
+    const checkbutton = document.createElement("button")
+    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkbutton.classList.add("check-button")
+    itemall.appendChild(checkbutton)
+    alert("ohh!!! Great you added your list😊😊")
+
+    const trashbutton = document.createElement("button")
+    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    trashbutton.classList.add("trash-button")
+    itemall.appendChild(trashbutton)
+
+    listtdl.appendChild(itemall)
+    inputtdl.value = ''
+}
+
+// checking and delete todoList 
+function okdel(e) {
+    const item = e.target
+
+    // check
+    if (item.classList[0] === 'check-button') {
+        const todolist = item.parentElement
+        todolist.classList.toggle('checklist')
+        alert("Task Completed!!😎😎")
+    }
+
+    // delete
+    if (item.classList[0] === 'trash-button') {
+        const todolist = item.parentElement
+        todolist.remove()
+        alert("oops!! your list is deleted 😐😐")
+    }
+}
+
+buttontdl.addEventListener('click', clickButton)
+listtdl.addEventListener('click', okdel)
